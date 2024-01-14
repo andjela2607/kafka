97949ec52f03174f2a592c7002129227a16f84d3
andjela2607 <andja011@gmail.com>
2024-01-14 18:54:43 +0100

    izlistavanje podataka sa API-a

--- front/src/App.js
+++ front/src/App.js
@@ -8,12 +8,20 @@ Chart.register(CategoryScale, LinearScale, PointElement, LineElement);
 const App = () => {
   const [data, setData] = useState([]);
   const [chartData, setChartData] = useState(null);
+  const [tempInC, setTempInC] = useState(null);
+  const [humidity, setHumidity] = useState(null);
+  const [pressure, setPressure] = useState(null);
 
   useEffect(() => {
     const socket = io('http://localhost:8000');
 
     socket.on('message', (message) => {
       const newData = JSON.parse(message);
+
+      if (newData.hasOwnProperty('temperature_in_c')) {
+        setTempInC(newData.temperature_in_c);
+      }
+
       setData((prevData) => [...prevData, newData]);
     });
 
@@ -27,19 +35,29 @@ const App = () => {
     if (data.length > 0) {
       const chartLabels = data.map((entry) => {
         const date = new Date(entry.dt * 1000);
-        return date.toLocaleDateString('en-GB'); // Formatiraj datum u "dd/MM/yy"
+        return date.toLocaleTimeString(); // Formatiraj datum u "dd/MM/yy"
       });
 
-      const chartTemperatureData = data.map((entry) => entry.main.temp+ Math.random()*2);
+      const chartTemperatureData = data.map((entry) => entry?.main?.temp);
+      const chartMaxTemperatureData = data.map((entry) => entry?.main?.temp_max);
+      const chartMinTemperatureData = data.map((entry) => entry?.main?.temp_min);
+      const chartFeelsLike = data.map((entry) => entry?.main?.feels_like)
+      const humidityArray = data.map((entry) => entry?.main?.humidity)
+      const pressureArray = data.map((entry) => entry?.main?.pressure)
 
+      setTempInC(chartTemperatureData[0] - 273.15);
+      setHumidity(humidityArray[0]);
+      setPressure(pressureArray[0]);
+      
       setChartData({
         labels: chartLabels,
+
         datasets: [
           {
             label: 'Temperatura',
             data: chartTemperatureData,
             fill: false,
-            borderColor: 'rgba(75,192,192,1)',
+            borderColor: '#000',
             tension: 0.1,
             elements: {
               point: {
@@ -50,6 +68,51 @@ const App = () => {
               },
             },
           },
+          {
+            label: 'Max Temperature',
+            data: chartMaxTemperatureData,
+            fill: false,
+            borderColor: '#ef0326',
+            tension: 0.1,
+            elements: {
+              point: {
+                radius: 0,
+              },
+              line: {
+                tension: 0.1,
+              }
+            }
+          },
+          {
+            label: 'Min Temperature',
+            data: chartMinTemperatureData,
+            fill: false,
+            borderColor: 'rgba(75,192,192,1)',
+            tension: 0.1,
+            elements: {
+              point: {
+                radius: 0,
+              },
+              line: {
+                tension: 0.1,
+              }
+            }
+          },
+          {
+            label: 'Feels Like',
+            data: chartFeelsLike,
+            fill: false,
+            borderColor: 'yellow',
+            tension: 0.1,
+            elements: {
+              point: {
+                radius: 0,
+              },
+              line: {
+                tension: 0.1,
+              }
+            }
+          }
         ],
       });
     }
@@ -59,6 +122,9 @@ const App = () => {
     <div>
       {/* Prikazivanje grafikona samo ako postoje podaci */}
       {chartData && <Line data={chartData} />}
+      <p>Temperature in Celsius: {tempInC?.toFixed(2)} °C</p>
+      {humidity && <p>Humidity: {humidity} % </p>}
+      {pressure && <p>Atmospheric pressure: {pressure} mbar</p>}
     </div>
   );
 };
